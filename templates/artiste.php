11601ea092468d76738f826ea3e88aa54232d514
QuentinJuhel <quentin.juhel@autrevue.fr>
2016-06-22 18:23:43 +0200

    mise en page

--- templates/artiste.php
+++ templates/artiste.php
@@ -6,45 +6,45 @@
   <div class="article" data-color="<?php echo $color?>">
 
 
+   <?php if($imagestart != 'undefined'): ?>
 
-      <div class="imagestart" >
-            <img src="<?php echo 'content/'.$folder.'/'.$imagestart ?>" alt="">
-      </div>
+        <div class="imagestart" >
+              <img src="<?php echo 'content/'.$folder.'/'.$imagestart ?>" alt="">
+        </div>
 
+   <?php endif ?>
  
-
-
-
     <div class="contenu" >
 
-      <div class="titre">
-        <?php echo $Parsedown->text($titre);?>
-      </div>
+        <div class="titre">
+          <?php echo $Parsedown->text($titre);?>
+        </div>
 
       <div class="row">
 
-        <div class="text small-6 columns">
-          <?php echo $Parsedown->text($text);?>
-        </div>
+          <div class="text small-6 columns">
+            <?php echo $Parsedown->text($text);?>
+          </div>
            
         
         
-        <div class="gallerie small-6 columns">
-        	<?php foreach($imagearray as $image){?>
-        			<img src="<?php echo 'content/'.$folder.'/'.$image ?>" alt="">
-        	<?php } ?>
-        </div>
+          <div class="gallerie small-6 columns">
+          	<?php foreach($imagearray as $image){?>
+          			<img src="<?php echo 'content/'.$folder.'/'.$image ?>" alt="">
+          	<?php } ?>
+          </div>
 
       </div>
 
     </div>
-
+  
+     <?php if($imageend != 'undefined'): ?>
 
         <div class="imageend">
               <img src="<?php echo 'content/'.$folder.'/'.$imageend ?>" alt="">
         </div>
+     <?php endif ?>
 
-
-
+    </div>
 
   </div>
--- templates/cover.php
+++ templates/cover.php
@@ -1,16 +1,16 @@
 <!-- Champs spécifiques -->
 <?php $subtitle = a::get($content, 'subtitle', 'undefined'); ?>
 
-<div class="cover break-after" style=" position:relative; " >
+<div class="coverbreak-after" >
 
 
 
-    <div class="imagecouv" style="position:absolute; z-index:1;">
+    <div class="imagecouv" >
     	<!--  faire une classe php pour cleaner tout ça - un truc qui retourne $image->url() par ex -->
     	<img src="<?php echo 'content/'.$folder.'/'.$imagearray[0] ?>" alt="">
     </div> 
 
-    <div class="titre" style=" position: absolute; z-index: 2; top:240px; padding-left:104px; color:#FFFFFF;">
+    <div class="titre" >
       <?php echo $Parsedown->text($titre);?>
     </div>
 
